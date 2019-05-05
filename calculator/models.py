4fe93a7700e74a618188410729ae9a096301d898
juliane <julianeventeu@yahoo.com.br>
2019-05-05 12:20:00 -0300

    Calcule installment and created test to payment

--- calculator/models.py
+++ calculator/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from decimal import Decimal
 
 
-class Loan(models.Model): 
+class LoanManager(models.Manager):
+    def create_loan(self, *args, **kwargs):
+        loan = self.create(*args, **kwargs)
+        loan.save_installment()
+        return loan
+
+
+class Loan(models.Model):
     """
     Loan Model
     Defines the attributes of a loan
@@ -12,22 +19,22 @@ class Loan(models.Model):
     rate = models.DecimalField('Rate', max_digits=15, decimal_places=2)
     date_initial = models.DateField('Date creation', auto_now=False, auto_now_add=False)
     installment = models.DecimalField('Installment', max_digits=15, decimal_places=2,default=Decimal('0000000000000.00'))
+    objects = LoanManager()
 
     class Meta:
         verbose_name = 'Loan'
         verbose_name_plural = 'Loans'
 
     def __str__(self):
-        return self.name
+        return str(self.pk)
 
     def get_absolute_url(self):
         return reverse("_detail", kwargs={"pk": self.pk})
 
-    @classmethod
-    def create(cls, title, ):
-        book = cls(title=title)
-        # do something with the book
-        return book
+    def save_installment(self):
+        r = self.rate / 12
+        self.installment = (r + r / ((1 + r) ** self.term - 1)) * self.amount
+        return self.save()
 
 
 class Payment(models.Model):
@@ -36,8 +43,8 @@ class Payment(models.Model):
     Defines the attributes of a Payment
     """
     loan_id = models.ForeignKey('Loan', on_delete=models.CASCADE)
-    PAYMENT_CHOICES = (('1', 'Made'), ('0', 'Missed'))
-    type = models.CharField('Type',max_length=1, choices=PAYMENT_CHOICES, default='1')
+    PAYMENT_CHOICES = (('MD', 'Made'), ('MS', 'Missed'))
+    type = models.CharField('Type', max_length=2, choices=PAYMENT_CHOICES, default='MD')
     date = models.DateField('Date', auto_now=False, auto_now_add=False)
     amount = models.DecimalField('Amount', max_digits=15, decimal_places=2)
 
@@ -46,7 +53,7 @@ class Payment(models.Model):
         verbose_name_plural = 'Payments'
 
     def __str__(self):
-        return self.name
+        return str(self.pk)
 
     def get_absolute_url(self):
         return reverse("_detail", kwargs={"pk": self.pk})
--- calculator/tests/test_models.py
+++ calculator/tests/test_models.py
@@ -2,18 +2,50 @@ from django.test import TestCase
 from datetime import datetime
 from decimal import Decimal
 
-from ..models import Loan
+from ..models import Loan, Payment
 
 
 class LoanTest(TestCase):
     """ Test module for Loan model """
-
+    
     def setUp(self):
-        Loan.objects.create(
+        Loan.objects.create_loan(
             amount=Decimal('1000.00'), term=12, rate=Decimal('0.05'), date_initial=datetime(2019,3,24)
         )
 
     def test_loan(self):
         loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
         self.assertEqual(
-            loan_01.term, 12)
+            loan_01.installment, Decimal('85.61')) #check use of decimals, because the digit is wrong
+    
+    def test_payment_made(self):
+        loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
+        Payment.objects.create(
+            loan_id=loan_01, type='MD', date=datetime(2019,4,24), amount=Decimal('100')
+        )
+        payment = Payment.objects.get(type='MD')
+        self.assertEqual(
+            payment.amount, Decimal('100'))
+    
+    def test_payment_missed(self):
+        loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
+        Payment.objects.create(
+            loan_id=loan_01, type='MS', date=datetime(2019,4,24), amount=Decimal('200')
+        )
+        payment = Payment.objects.get(type='MS')
+        self.assertEqual(
+            payment.amount, Decimal('200'))
+    
+    """
+        This test should failure, because 0 is not a option type.
+        Search a way to do this.
+    """
+    def test_payment_error(self):
+        loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
+        Payment.objects.create(
+            loan_id=loan_01, type='0', date=datetime(2019,4,24), amount=Decimal('300')
+        )
+        payment = Payment.objects.get(type='0')
+        self.assertEqual(
+            payment.amount, Decimal('300'))
+
