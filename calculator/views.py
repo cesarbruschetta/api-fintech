821d82e0aa11a0881bc4696b553f8a1fd9260b89
juliane <julianeventeu@yahoo.com.br>
2019-05-06 20:17:41 -0300

    test view

--- calculator/views.py
+++ calculator/views.py
@@ -19,5 +19,4 @@ def post_loans(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
