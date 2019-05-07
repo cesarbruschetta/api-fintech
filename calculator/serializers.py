edb058a114f1f2a4512e1855febba94e1ba8e413
juliane <julianeventeu@yahoo.com.br>
2019-05-06 22:20:04 -0300

    Representation of post

--- calculator/serializers.py
+++ calculator/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from decimal import Decimal
 
 from .models import Loan, Payment
 
@@ -6,7 +7,13 @@ from .models import Loan, Payment
 class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
-        fields = '__all__'
+        fields = ('__all__')
+    
+    def to_representation(self, obj):
+        return {
+            'id': str(obj.id),
+            'installment': float(obj.installment)
+        }
 
 
 class PaymentSerializer(serializers.ModelSerializer):
