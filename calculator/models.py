6162863c80563634dc5f5fe39f70a19118a1b891
Cesar Augusto <cesarabruschetta@gmail.com>
2019-05-08 17:04:29 +0000

    Create model Client and travis.yml #14

--- calculator/models.py
+++ calculator/models.py
@@ -3,44 +3,72 @@ from django.db import models
 from decimal import Decimal, ROUND_DOWN
 from datetime import datetime, timezone
 
+
+class Client(models.Model):
+    """
+    Client Model
+    Defines the attributes of a client
+    """
+
+    name = models.CharField("Name", max_length=255)
+    surname = models.CharField("Surname", max_length=255)
+    email = models.CharField("E-mail", max_length=255)
+    telephone = models.CharField("Telephone", max_length=255)
+    cpf = models.CharField("CPF", max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Loan(models.Model):
     """
     Loan Model
     Defines the attributes of a loan
     """
-    amount = models.DecimalField('Amount', max_digits=15, decimal_places=2,
-                                 validators=[
-                                  RangeMinValueValidator(Decimal('0.01'))
-                                 ])
-    term = models.IntegerField('Term',
-                               validators=[
-                                RangeMinValueValidator(1)
-                               ])
-    rate = models.DecimalField('Rate', max_digits=15, decimal_places=2,
-                               validators=[
-                                RangeMinValueValidator(Decimal('0.01'))
-                               ])
-    date_initial = models.DateTimeField('Date creation', auto_now=False, auto_now_add=False)
+
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    amount = models.DecimalField(
+        "Amount",
+        max_digits=15,
+        decimal_places=2,
+        validators=[RangeMinValueValidator(Decimal("0.01"))],
+    )
+    term = models.IntegerField("Term", validators=[RangeMinValueValidator(1)])
+    rate = models.DecimalField(
+        "Rate",
+        max_digits=15,
+        decimal_places=2,
+        validators=[RangeMinValueValidator(Decimal("0.01"))],
+    )
+    date_initial = models.DateTimeField(
+        "Date creation", auto_now=False, auto_now_add=False
+    )
 
     class Meta:
-        verbose_name = 'Loan'
-        verbose_name_plural = 'Loans'
+        verbose_name = "Loan"
+        verbose_name_plural = "Loans"
 
     def __str__(self):
         return str(self.pk)
-    
+
     @property
     def installment(self):
         r = self.rate / 12
         installment = (r + r / ((1 + r) ** self.term - 1)) * self.amount
-        return installment.quantize(Decimal('.01'), rounding=ROUND_DOWN)
-    
+        return installment.quantize(Decimal(".01"), rounding=ROUND_DOWN)
+
     def get_balance(self, date_base=datetime.now().astimezone(tz=timezone.utc)):
         try:
-            payments = self.payment_set.filter(type='MD', date__lte=date_base).values('amount')
-            return self.amount - sum([payment['amount'] for payment in payments])
+            payments = self.payment_set.filter(type="MD", date__lte=date_base).values(
+                "amount"
+            )
+            return self.amount - sum([payment["amount"] for payment in payments])
         except:
-            return Decimal('0')
+            return Decimal("0")
 
 
 class Payment(models.Model):
@@ -48,19 +76,21 @@ class Payment(models.Model):
     Payment Model
     Defines the attributes of a Payment
     """
-    loan_id = models.ForeignKey('Loan', on_delete=models.CASCADE)
-    PAYMENT_CHOICES = (('MD', 'Made'), ('MS', 'Missed'))
-    type = models.CharField('Type', max_length=2, choices=PAYMENT_CHOICES, default='MD')
-    date = models.DateTimeField('Date', auto_now=False, auto_now_add=False)
-    amount = models.DecimalField('Amount', max_digits=15, decimal_places=2,
-                                 validators=[
-                                    RangeMinValueValidator(Decimal('0.01'))
-                                 ])
+
+    loan_id = models.ForeignKey("Loan", on_delete=models.CASCADE)
+    PAYMENT_CHOICES = (("MD", "Made"), ("MS", "Missed"))
+    type = models.CharField("Type", max_length=2, choices=PAYMENT_CHOICES, default="MD")
+    date = models.DateTimeField("Date", auto_now=False, auto_now_add=False)
+    amount = models.DecimalField(
+        "Amount",
+        max_digits=15,
+        decimal_places=2,
+        validators=[RangeMinValueValidator(Decimal("0.01"))],
+    )
 
     class Meta:
-        verbose_name = 'Payment'
-        verbose_name_plural = 'Payments'
+        verbose_name = "Payment"
+        verbose_name_plural = "Payments"
 
     def __str__(self):
         return str(self.pk)
-
--- calculator/tests/test_models.py
+++ calculator/tests/test_models.py
@@ -2,76 +2,148 @@ from django.test import TestCase
 from datetime import datetime, timezone
 from decimal import Decimal
 
-from ..models import Loan, Payment
+from ..models import Loan, Payment, Client
+
+
+class ClientTest(TestCase):
+    """ Test module for Client model """
+
+    @classmethod
+    def setUpClass(cls):
+        super(ClientTest, cls).setUpClass()
+        Client.objects.create(
+            name="Ian Marcos",
+            surname="Carvalho",
+            email="ianmarcoscarvalho@gmail.com.br",
+            telephone="(91) 3794-6863",
+            cpf="512.811.038-96",
+        )
+
+    def test_client(self):
+        loan_01 = Client.objects.all()[0]
+        self.assertEqual(loan_01.name, "Ian Marcos")
 
 
 class LoanTest(TestCase):
     """ Test module for Loan and Payment model """
-    
-    def setUp(self):
+
+    @classmethod
+    def setUpClass(cls):
+        super(LoanTest, cls).setUpClass()
+
+        client = Client.objects.create(
+            name="Ian Marcos",
+            surname="Carvalho",
+            email="ianmarcoscarvalho@gmail.com.br",
+            telephone="(91) 3794-6863",
+            cpf="204.421.210-24",
+        )
         Loan.objects.create(
-            amount=Decimal('1000.00'), term=12, rate=Decimal('0.05'), date_initial=datetime(2019,3,24,11,30).astimezone(tz=timezone.utc)
+            client=client,
+            amount=Decimal("1001.00"),
+            term=12,
+            rate=Decimal("0.05"),
+            date_initial=datetime(2019, 3, 24, 11, 30).astimezone(tz=timezone.utc),
         )
 
+    def setUp(self):
+        self.loan_01 = Loan.objects.get(amount=Decimal("1001.00"))
+
     def test_loan(self):
-        loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
-        self.assertEqual(
-            loan_01.installment, Decimal('85.60'))
-    
+        self.assertEqual(self.loan_01.installment, Decimal("85.69"))
+
     def test_payment_made(self):
-        loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
         Payment.objects.create(
-            loan_id=loan_01, type='MD', date=datetime(2019,4,24).astimezone(tz=timezone.utc), amount=Decimal('100')
+            loan_id=self.loan_01,
+            type="MD",
+            date=datetime(2019, 4, 24).astimezone(tz=timezone.utc),
+            amount=Decimal("100"),
         )
-        payment = Payment.objects.get(type='MD')
-        self.assertEqual(
-            payment.amount, Decimal('100'))
-    
+        payment = Payment.objects.get(type="MD")
+        self.assertEqual(payment.amount, Decimal("100"))
+
     def test_payment_missed(self):
-        loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
         Payment.objects.create(
-            loan_id=loan_01, type='MS', date=datetime(2019,4,24).astimezone(tz=timezone.utc), amount=Decimal('200')
+            loan_id=self.loan_01,
+            type="MS",
+            date=datetime(2019, 4, 24).astimezone(tz=timezone.utc),
+            amount=Decimal("200"),
         )
-        payment = Payment.objects.get(type='MS')
-        self.assertEqual(
-            payment.amount, Decimal('200'))
-    
+        payment = Payment.objects.get(type="MS", loan_id=self.loan_01)
+        self.assertEqual(payment.amount, Decimal("200"))
+
     """
         This test should failure, because 0 is not a option type.
         Search a way to do this.
     """
+
     def test_payment_error(self):
-        loan_01 = Loan.objects.get(amount=Decimal('1000.00'))
         Payment.objects.create(
-            loan_id=loan_01, type='0', date=datetime(2019,4,24).astimezone(tz=timezone.utc), amount=Decimal('300')
+            loan_id=self.loan_01,
+            type="0",
+            date=datetime(2019, 4, 24).astimezone(tz=timezone.utc),
+            amount=Decimal("300"),
         )
-        payment = Payment.objects.get(type='0')
-        self.assertEqual(
-            payment.amount, Decimal('300'))
+        payment = Payment.objects.get(type="0", loan_id=self.loan_01)
+        self.assertEqual(payment.amount, Decimal("300"))
 
 
 class BalanceTest(TestCase):
     """ Test module for Balance model """
-    
-    def setUp(self):
-        self.loan_01 = Loan.objects.create(
-            amount=Decimal('1000.00'), term=12, rate=Decimal('0.05'), date_initial=datetime(2019,3,24,11,30).astimezone(tz=timezone.utc)
+
+    @classmethod
+    def setUpClass(cls):
+        super(BalanceTest, cls).setUpClass()
+
+        client = Client.objects.create(
+            name="Ian Marcos",
+            surname="Carvalho",
+            email="ianmarcoscarvalho@gmail.com.br",
+            telephone="(91) 3794-6863",
+            cpf="496.034.860-78",
+        )
+
+        cls.loan_01 = Loan.objects.create(
+            client=client,
+            amount=Decimal("1000.00"),
+            term=12,
+            rate=Decimal("0.05"),
+            date_initial=datetime(2019, 3, 24, 11, 30).astimezone(tz=timezone.utc),
         )
         Payment.objects.create(
-            loan_id=self.loan_01, type='MD', date=datetime(2019,4,24).astimezone(tz=timezone.utc), amount=Decimal('200')
+            loan_id=cls.loan_01,
+            type="MD",
+            date=datetime(2019, 4, 24).astimezone(tz=timezone.utc),
+            amount=Decimal("200"),
         )
         Payment.objects.create(
-            loan_id=self.loan_01, type='MD', date=datetime(2019,4,24).astimezone(tz=timezone.utc), amount=Decimal('200')
+            loan_id=cls.loan_01,
+            type="MD",
+            date=datetime(2019, 4, 24).astimezone(tz=timezone.utc),
+            amount=Decimal("200"),
         )
         Payment.objects.create(
-            loan_id=self.loan_01, type='MS', date=datetime(2019,4,24).astimezone(tz=timezone.utc), amount=Decimal('200')
+            loan_id=cls.loan_01,
+            type="MS",
+            date=datetime(2019, 4, 24).astimezone(tz=timezone.utc),
+            amount=Decimal("200"),
         )
-    
+
     def test_balance_loan_valid(self):
-        self.assertEqual(self.loan_01.get_balance(date_base=datetime(2019,4,25).astimezone(tz=timezone.utc)), Decimal('600'))
+        self.assertEqual(
+            self.loan_01.get_balance(
+                date_base=datetime(2019, 4, 25).astimezone(tz=timezone.utc)
+            ),
+            Decimal("600"),
+        )
 
     def test_balance_without_payments(self):
-        self.assertEqual(self.loan_01.get_balance(date_base=datetime(2019,3,25).astimezone(tz=timezone.utc)), self.loan_01.amount)
-    
+        self.assertEqual(
+            self.loan_01.get_balance(
+                date_base=datetime(2019, 3, 25).astimezone(tz=timezone.utc)
+            ),
+            self.loan_01.amount,
+        )
+
     def test_balance_loan_without_date_base(self):
-        self.assertEqual(self.loan_01.get_balance(), Decimal('600'))
+        self.assertEqual(self.loan_01.get_balance(), Decimal("600"))
