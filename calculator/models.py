023c256b0d82c55962f5a70eb7a744b7a0d8f9cc
Dario Diniz <dariodg@gmail.com>
2019-05-10 14:17:29 -0300

    Added validators and new installment parameters

--- calculator/models.py
+++ calculator/models.py
@@ -2,6 +2,9 @@ from django.contrib.postgres.validators import RangeMinValueValidator
 from django.db import models
 from decimal import Decimal, ROUND_DOWN
 from datetime import datetime, timezone
+from copy import deepcopy
+from django.forms import DecimalField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Client(models.Model):
@@ -21,6 +24,8 @@ class Client(models.Model):
         verbose_name = 'Client'
         verbose_name_plural = 'Clients'
 
+    def get_history(self):
+        pass
 
 
 class Loan(models.Model):
@@ -28,29 +33,30 @@ class Loan(models.Model):
     Loan Model
     Defines the attributes of a loan
     """
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    amount = models.DecimalField('Amount', max_digits=15, decimal_places=2)
-    term = models.IntegerField('Term')
-    rate = models.DecimalField('Rate', max_digits=15, decimal_places=2)
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
+    amount = models.DecimalField('Amount', max_digits=15, decimal_places=2,validators=[MinValueValidator(0.01)])
+    term = models.IntegerField('Term', validators=[MinValueValidator(1)])
+    rate = models.DecimalField('Rate', max_digits=15, decimal_places=2, validators=[MinValueValidator(0.01)])
     date_initial = models.DateTimeField('Date creation', auto_now=False, auto_now_add=False)
 
-   
-    def client_payment_history(self):
+    def installment_adjustment(self):
         loans_history = self.client.loan_set.all()
         missed_payments = sum([Payment.objects.filter(loan_id=loan, status='MS').count() for loan in loans_history])
-        debt = sum([loan.get_balance() for loan in loans_history])
-        bias = 0 
-        if debt == 0:
-            if missed_payments == 0:
-                bias = -0.02
-            elif 0 < missed_payments <= 3:
-                bias = 0.04
-        return Decimal(bias)
+        if len(loans_history) > 1:
+            existing_debt = sum([loans_history[i].get_balance() for i in range(len(loans_history)-1)])
+            adjustment = 0
+            if existing_debt == 0:
+                if missed_payments == 0:
+                    adjustment = -0.02
+                elif 0 < missed_payments <= 3:
+                    adjustment = 0.04
+                return Decimal(adjustment)
+        return Decimal(0)
 
     @property
     def installment(self):
         r = self.rate / self.term
-        installment = (r + r / ((1 + r) ** self.term - 1)) * self.amount * (1 + self.client_payment_history())
+        installment = (r + r / ((1 + r) ** self.term - 1)) * self.amount * (1 + self.installment_adjustment())
         return installment.quantize(Decimal('.01'), rounding=ROUND_DOWN)
     
     def get_balance(self, date_base=datetime.now().astimezone(tz=timezone.utc)):
@@ -62,6 +68,10 @@ class Loan(models.Model):
 
     def __repr__(self):
         return f'Loan(loan_id={self.id}, amount={self.amount}, term={self.term}, rate={self.rate}, date_initial={self.date_initial})'
+
+    def __str__(self):
+        return f'id: {self.pk} | {self.client}'
+
 
     class Meta:
         verbose_name = 'Loan'
