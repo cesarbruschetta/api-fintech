abd29a7e165617d272767184de5f710ceca27350
juliane <julianeventeu@yahoo.com.br>
2019-05-12 21:33:10 -0300

    Changed url() to path()

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,26 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    url(
-        r'^v1/clients/$',
-        views.post_clients,
-        name='post_clients'
-    ),
-    url(
-        r'^v1/loans/$',
-        views.post_loans,
-        name='post_loans'
-    ),
-    url(
-        r'^v1/loans/(?P<pk>[0-9]+)/payments$',
-        views.post_payments,
-        name='post_payments'
-    ),
-    url(
-        r'^v1/loans/(?P<pk>[0-9]+)/balance$',
-        views.get_balance,
-        name='get_balance'
-    ),
+    path('v1/clients/', views.post_clients, name='post_clients'),
+    path('v1/loans/', views.post_loans, name='post_loans'),
+    path('v1/loans/<int:pk>/payments', views.post_payments, name='post_payments'),
+    path('v1/loans/<int:pk>/balance', views.get_balance, name='get_balance'),
 ]
