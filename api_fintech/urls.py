ffca70e94e138f6a4b340d1725fa5b46089cf98f
Juliane Regina Venteu <juliane.venteu@sp01.local>
2019-05-14 12:33:59 -0300

    pep8

--- api_fintech/urls.py
+++ api_fintech/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
-
 urlpatterns = [
     path('', include('calculator.urls')),
     path('admin/', admin.site.urls),
