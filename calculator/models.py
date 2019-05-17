7f5e1f12494868785caaa0984d3cf33f4e40ed91
Dario Diniz <43078060+dariodinizg@users.noreply.github.com>
2019-05-17 19:07:12 -0300

    Update models.py
    Add: 
    - Field for rate adjust
    - Format constrains for decimal
    - Changed term field to decimal field.

--- calculator/models.py
+++ calculator/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.forms import DecimalField
 from django.core.validators import MinValueValidator
 
-from decimal import Decimal, ROUND_DOWN
+from decimal import Decimal, ROUND_DOWN, localcontext
 from datetime import datetime, timezone
 
 
 def increment_loan_id():
-        id_loan = '{:015d}'.format(Loan.objects.count() + 1)
-        return '{}-{}-{}-{}'.format(id_loan[:3], id_loan[3:7], id_loan[7:11], id_loan[11:15])
+    id_loan = '{:015d}'.format(Loan.objects.count() + 1)
+    return '{}-{}-{}-{}'.format(id_loan[:3], id_loan[3:7], id_loan[7:11], id_loan[11:15])
 
 
 class Client(models.Model):
@@ -47,7 +47,8 @@ class Loan(models.Model):
     Loan Model
     Defines the attributes of a loan
     """
-    id = models.CharField(max_length=19, default=increment_loan_id, editable=False, primary_key=True)
+    id = models.CharField(
+        max_length=19, default=increment_loan_id, editable=False, primary_key=True)
     client = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
     amount = models.DecimalField(
         "Amount",
@@ -55,19 +56,33 @@ class Loan(models.Model):
         decimal_places=2,
         validators=[MinValueValidator(Decimal("0.01"))],
     )
-    term = models.IntegerField("Term", validators=[MinValueValidator(1)])
+    date_initial = models.DateTimeField(
+        "Date creation", auto_now=False, auto_now_add=False
+    )
+    term = models.DecimalField(
+        "Rate",
+        max_digits=2,
+        decimal_places=0,
+        validators=[MinValueValidator(Decimal("1"))],
+    )
     rate = models.DecimalField(
         "Rate",
         max_digits=15,
         decimal_places=2,
         validators=[MinValueValidator(Decimal("0.01"))],
     )
-    date_initial = models.DateTimeField(
-        "Date creation", auto_now=False, auto_now_add=False
+    rate_adjust = models.DecimalField(
+        "Rate Auto Adjust",
+        editable=False,
+        default=Decimal('0.00'),
+        max_digits=15,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.01"))],
     )
     instalment = models.DecimalField(
         "Instalment",
-        default=Decimal('0.0'),
+        editable=False,
+        default=Decimal('0.00'),
         max_digits=15,
         decimal_places=2,
         validators=[MinValueValidator(Decimal("0.01"))],
@@ -81,22 +96,29 @@ class Loan(models.Model):
                 for loan in loans_history
             ]
         )
-        adjustment = Decimal('0')
+        adjustment = Decimal('0.00')
         if len(loans_history) >= 1:
             if missed_payments == 0:
-                adjustment = Decimal("-0.02")
+                adjustment = Decimal("-0.002")
             elif 0 < missed_payments <= 3:
-                adjustment = Decimal("0.04")
-        return Decimal(adjustment)
+                adjustment = Decimal("0.004")
+        return adjustment
 
     def calculate_instalment(self):
-        r = (self.rate + self._rate_adjustment())/ self.term
-        instalment = (
-            (r + r / ((1 + r) ** self.term - 1))
-            * self.amount
-            #* self._rate_adjustment()
-        )
-        return instalment.quantize(Decimal(".01"), rounding=ROUND_DOWN)
+        _2places = Decimal("0.00")
+
+        with localcontext() as ctx:
+            ctx.rounding = ROUND_DOWN
+            rate = Decimal(f"{self.rate}")
+            term = Decimal(f"{self.term}")
+            amount = Decimal(f"{self.amount}")
+            r = (rate + self._rate_adjustment()) / term
+            instalment = ((
+                r
+                + r
+                / (ctx.power((1 + r), term)
+                   - 1)) * amount).quantize(_2places)
+        return instalment
 
     def get_balance(self, date_base=datetime.now().astimezone(tz=timezone.utc)):
         try:
@@ -108,7 +130,8 @@ class Loan(models.Model):
             return Decimal("0")
 
     def save(self, *args, **kwargs):
-        self.instalment += self.calculate_instalment()
+        self.rate_adjustment = self._rate_adjustment()
+        self.instalment = self.calculate_instalment()
         super(Loan, self).save(*args, **kwargs)
 
     class Meta:
@@ -127,11 +150,12 @@ class Payment(models.Model):
     PAYMENT_CHOICES = (('made', 'made'), ('missed', 'missed'))
 
     loan_id = models.ForeignKey('Loan', on_delete=models.CASCADE)
-    status = models.CharField('status', db_column='type', max_length=6, choices=PAYMENT_CHOICES)
+    status = models.CharField(
+        'status', db_column='type', max_length=6, choices=PAYMENT_CHOICES)
     date = models.DateTimeField('Date', auto_now=False, auto_now_add=False)
     amount = models.DecimalField('Amount', max_digits=15, decimal_places=2,
                                  validators=[
-                                    MinValueValidator(Decimal("0.01"))
+                                     MinValueValidator(Decimal("0.01"))
                                  ])
 
     class Meta:
